c20dac9c6284e0fbaaafe7b648fce70dffa8f14b
Byorn De Silva <byorn.desilva@gmail.com>
2023-03-01 14:52:22 +1100

    Fixed hydration error. Testing Next/Image to resize on Mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,11 @@ export default function Home() {
       <Box sx={{ overflowX: 'hidden' }}>
 
               <Container>
-                  <Image src={"/byorns-playground-logo.png"} width={500} height={80} alt={'myimage'}/>
-                  <p> <h3>Mar 01 - 2023 </h3> I have been keen on researching on NextJS 13, and deploying it to AWS Amplify.
+                  <Image src={"/byorns-playground-logo.png"}  width={300} height={100}
+                       alt={'myimage'}/>
+                  <h3>Mar 01 - 2023 </h3> I have been keen on researching on NextJS 13, and deploying it to AWS Amplify.
                         I wanted to start by building a clone of of Airtasker.com. <br/>
-                       <Link href={'/front-page'}>Click here</Link> to find a WIP of that page.</p>
+                       <Link href={'/front-page'}>Click here</Link> to find a WIP of that page.
               </Container>
       </Box>
   )
