1f7cc7b2405bf0654f4050211fa0fd462f30bc69
Byorn De Silva <byorn.desilva@gmail.com>
2024-06-26 19:54:27 +1000

    Next Auth login

--- src/components/header-page-contents.tsx
+++ src/components/header-page-contents.tsx
@@ -15,8 +15,22 @@ export default function HeaderPageContents(){
                         Projects</a>
                     <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Projects
                         I have worked on</a>
-                    <a href="#" style={{width: '500px'}} className="pb-6"> A breif overview of projects I have worked
+                    <a href="#" style={{width: '500px'}} className="pb-6"> A brief overview of projects I have worked
                         on</a>
+                </div>
+            </article>
+            <br/>
+            <article className="flex flex-col shadow max-w-full hover:border hover:border-blue-500 p-4" onClick={() => {
+                location.href = 'https://www.byorns-playground.com/comments'
+            }}>
+
+                <div className="bg-white flex flex-col justify-start  mx-auto max-w-full py-6 px-12">
+                    <a href="https://cms.byorns-playground.com/projects/a-quick-overview-of-projects-i-worked-on-commercially"
+                       className="text-blue-700 text-sm font-bold uppercase pb-4">
+                        Authentication and Authorisation</a>
+                    <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Next Auth  </a>
+                    <a href="#" style={{width: '500px'}} className="pb-6"> Using NextAuth to  login with Github or any other social provider, and protecing pages where a user is not logged in ...
+                        </a>
                 </div>
             </article>
             <br/>
